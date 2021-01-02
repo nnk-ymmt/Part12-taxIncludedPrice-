1faf6e8ee3a59baaf578b7f3f302a7f802061e8f
nnk-ymmt <garden.n1234567890@gmail.com>
2021-01-02 19:59:52 +0900

    VC更新

--- Part12-taxIncludedPrice-/ViewController.swift
+++ Part12-taxIncludedPrice-/ViewController.swift
@@ -11,13 +11,21 @@ final class ViewController: UIViewController {
 
     @IBOutlet private weak var taxExcludedPriceTextField: UITextField!
     @IBOutlet private weak var taxRateTextField: UITextField!
-    @IBOutlet private weak var taxIncludedPriceTextField: UILabel!
+    @IBOutlet private weak var taxIncludedPriceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let taxRate = UserDefaults.standard.integer(forKey: "taxRate")
+        taxRateTextField.text = String(taxRate)
     }
 
-
+    @IBAction private func calculate(_ sender: Any) {
+        guard let taxExcludedPrice = Int(taxExcludedPriceTextField.text ?? ""),
+           let taxRate = Float(taxRateTextField.text ?? "") else {
+            return
+        }
+        let taxIncludedPrice = Int(Float(taxExcludedPrice) * ( 1 + taxRate / 100))
+        taxIncludedPriceLabel.text = String(taxIncludedPrice)
+        UserDefaults.standard.set(taxRate, forKey: "taxRate")
+    }
 }
-
